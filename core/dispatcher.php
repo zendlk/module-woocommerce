33650b30ce6301eabd0eb856edf389bf0d053701
Shyamin Ayesh <me@shyamin.com>
2021-07-26 00:32:45 +0530

    customer SMS notifications

--- core/dispatcher.php
+++ core/dispatcher.php
@@ -17,7 +17,8 @@ class Dispatcher {
 	public function __construct() {
 		$this->Config = Config::create([
 			"token" => get_option($this->prefix."_api_token"),
-			"sender" => get_option($this->prefix."_sender_id")
+			"sender" => get_option($this->prefix."_sender_id"),
+			"version" => "2.0"
 		]);
 	}
 
@@ -29,10 +30,9 @@ class Dispatcher {
 	 * administrator notifications
 	 */
 	public function OnEvent_StatusChange($order_id, $previous_status, $new_status) {
-		if (get_option($this->prefix."_sms_template_status_".$new_status) == "yes") { self::Dispatch_StaffNotifications($order_id, $new_status); }
+		self::Dispatch_CustomerNotifications($order_id, $new_status);
 		if ($new_status == "processing" &&  get_option($this->prefix."_administrator_notification_status") == "yes") { $this->Dispatch_StaffNotifications($order_id, $new_status); }
 	}
-
 
 	/**
 	 * This method will dispatch the formatted notification to
@@ -84,8 +84,59 @@ class Dispatcher {
 	}
 
 
+	/**
+	 * Here we dispatch notification sms to customers
+	 * according to the information submited to the
+	 * function by woocom implementation.
+	 */
 	private function Dispatch_CustomerNotifications($id, $status) {
-		error_log("Dispatcher::Dispatch()");
+
+		/**
+		 * Check if the requested message template is enabled
+		 * by the configuration.
+		 */
+		$key = str_replace("-", "_", $status);
+		if (get_option($this->prefix."_sms_template_status_".$key) == "yes"):
+
+			/**
+			 * Check if the message template for the requested
+			 * status is not empty and fall back to the default
+			 * message if the specified status template is empty
+			 */
+			$message = get_option($this->prefix."_sms_template_".$key);
+			if (empty($message)):
+				$message = get_option($this->prefix."_sms_template_default");
+			endif;
+
+			$Order = new WC_Order($id);
+			$short_code_informations = [
+				"{{shop_name}}"			=> get_bloginfo('name'),
+				"{{order_id}}"			=> $Order->get_order_number(),
+				"{{order_amount}}"		=> $Order->get_total(),
+				"{{order_status}}"		=> ucfirst($Order->get_status()),
+				"{{first_name}}"		=> ucfirst($Order->billing_first_name),
+				"{{last_name}}"			=> ucfirst($Order->billing_last_name),
+				"{{billing_city}}"		=> ucfirst($Order->billing_city),
+				"{{customer_phone}}"	=> $Order->billing_phone,
+			];
+
+			$message = str_replace(array_keys($short_code_informations), $short_code_informations, $message);
+
+			/**
+			 * Now we have to check and format the mobile number
+			 * of the recipient according to E164 standard format
+			 * before handing over to the Zend API.
+			 */
+			preg_match_all('/^(?:0|94|\+94|0094|\+940)?(?:(?P<area>11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|91)(?P<land_carrier>0|2|3|4|5|7|9)|7(?P<mobile_carrier>0|1|2|4|5|6|7|8)\d)\d{6}$/', $Order->billing_phone, $matches, PREG_SET_ORDER, 0);
+
+			// SEND
+			SMS::compose($this->Config, [
+				"to" => ["+94".substr($matches[0][0], -9)],
+				"text" => $message,
+			])->send();
+
+		endif;
+
 	}
 
 }
